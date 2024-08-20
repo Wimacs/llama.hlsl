af06e192ca695c18219bad9a01e5cbc7caeca268
Wimacs <wuyilong2000@outlook.com>
2024-08-20 21:13:17 +0800

    initial commit

---
+++ CMakeFiles/llama.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/llama.dir/main.cpp.o"
+  "CMakeFiles/llama.dir/main.cpp.o.d"
+  "llama"
+  "llama.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/llama.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
