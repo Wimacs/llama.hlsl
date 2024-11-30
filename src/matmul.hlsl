9fa88412cfc1959e13e6cca2b3fcafc2fa052c45
Wimacs <wuyilong2000@outlook.com>
2024-11-30 22:19:12 +0800

    modify shader

--- src/matmul.hlsl
+++ src/matmul.hlsl
@@ -7,6 +7,22 @@ RWStructuredBuffer<float> matrixA : register(u0);
 RWStructuredBuffer<float> matrixB : register(u1);
 RWStructuredBuffer<float> result : register(u2);
 
+
+StructuredBuffer<float> token_embedding_table : register(t0);
+StructuredBuffer<float> rms_att_weight : register(t1);
+StructuredBuffer<float> rms_ffn_weight : register(t2);
+StructuredBuffer<float> wq : register(t3);
+StructuredBuffer<float> wk : register(t4);
+StructuredBuffer<float> wv : register(t5);
+StructuredBuffer<float> wo : register(t6);
+StructuredBuffer<float> w1 : register(t7);
+StructuredBuffer<float> w2 : register(t8);
+StructuredBuffer<float> w3 : register(t9);
+StructuredBuffer<float> rms_final_weight : register(t10);
+StructuredBuffer<float> wcls : register(t11);
+
+
+
 [numthreads(32, 32, 1)]
 void CSMain(uint3 DTid : SV_DispatchThreadID)
 {
@@ -19,5 +35,5 @@ void CSMain(uint3 DTid : SV_DispatchThreadID)
         sum += matrixA[DTid.y * MATRIX_WIDTH + k] * matrixB[k * MATRIX_WIDTH + DTid.x];
     }
     
-    result[DTid.y * MATRIX_WIDTH + DTid.x] = matrixB[DTid.x];
+    result[DTid.y * MATRIX_WIDTH + DTid.x] = rms_final_weight[DTid.x];
 }
